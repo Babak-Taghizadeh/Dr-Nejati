507653b6066076545cdfc4993858192042899685
Babak Taghizadeh <babak.tz98@gmail.com>
2024-10-10 17:36:10 +0330

    default locale fixed

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,10 @@
 import createMiddleware from "next-intl/middleware";
 import { routing } from "./i18n/routing";
 
-export default createMiddleware(routing);
+export default createMiddleware(routing, {
+  localeDetection: false,
+});
 
 export const config = {
-  matcher: ['/', '/(fa|en|tr)/:path*']
+  matcher: ["/", "/(fa|en|tr)/:path*"],
 };
