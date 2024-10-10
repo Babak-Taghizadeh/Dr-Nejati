81339030641c6cbba9405262d913004d6962c399
Babak Taghizadeh <babak.tz98@gmail.com>
2024-10-10 16:19:51 +0330

    headline modified

--- src/components/Headline.tsx
+++ src/components/Headline.tsx
@@ -1,13 +1,17 @@
-import { useTranslations } from "next-intl"
+import { useTranslations } from "next-intl";
 
 const Headline = () => {
-    const t = useTranslations("HomePage")
+  const t = useTranslations("HomePage");
   return (
-      <div className="flex flex-col mb-8 items-center justify-center px-6 gap-4 lg:gap-6">
-          <h1 className="text-2xl font-semibold text-primary sm:text-4xl lg:text-6xl">{t("headline")}</h1>
-          <h2 className="text-center text-balance font-semibold text-primary sm:text-xl lg:text-2xl">{t("subHeadline")}</h2>
+    <div className="mb-8 flex flex-col items-center justify-center gap-2 px-6 lg:gap-6">
+      <h1 className="text-primary text-center text-lg font-semibold sm:text-4xl lg:text-5xl">
+        {t("headline")}
+      </h1>
+      <h2 className="text-primary text-balance text-center text-base sm:text-xl lg:text-2xl">
+        {t("subHeadline")}
+      </h2>
     </div>
-  )
-}
+  );
+};
 
-export default Headline
+export default Headline;
